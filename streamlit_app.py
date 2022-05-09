925fa12358fb9a66bf911d2f99d6e5af15cbee36
gokuls92 <105046988+gokuls92@users.noreply.github.com>
2022-05-09 19:25:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,15 +41,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-#streamlit.stop()
-#import snowflake.connector
-
-
-
-
-
-
-
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
   with  my_cnx.cursor() as my_cur:
@@ -59,6 +50,16 @@ def get_fruit_load_list():
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
+streamlit.stop()
+#import snowflake.connector
+
+
+
+
+
+
+
+
 add_my_fruit=streamlit.text_input("What fruit would you like to add?","jackfruit")
 streamlit.write("Thanks for adding",add_my_fruit )
 
